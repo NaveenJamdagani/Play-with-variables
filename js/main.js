313fab8ef293ddcda016f6af1e95926cd16ba0ef
Anqush Jamdagani <njamdagani@gmail.com>
2017-09-03 18:29:25 +0530

    project completed

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ var inputs = document.querySelectorAll(".controls input");
 console.log(inputs);
 
 var variableChange = function () {
+	if(this.name === "base")
+	{
+		document.documentElement.style.setProperty(`--${this.name}`,this.value);
+	}
+	else
+	{
+		document.documentElement.style.setProperty(`--${this.name}`,this.value + "px");
+	}
 	console.log(this.name);
 }
 
-inputs.forEach(input => input.addEventListener("change",variableChange));
 inputs.forEach(input => input.addEventListener("input",variableChange));
 
 
