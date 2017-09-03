292006e1426d256a6b1b9d888db8db17a546cea6
Anqush Jamdagani <njamdagani@gmail.com>
2017-09-03 18:09:49 +0530

    input values

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 var inputs = document.querySelectorAll(".controls input");
+
 console.log(inputs);
+
+var variableChange = function () {
+	console.log(this.name);
+}
+
+inputs.forEach(input => input.addEventListener("change",variableChange));
+inputs.forEach(input => input.addEventListener("input",variableChange));
+
+
 
